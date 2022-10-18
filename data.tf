247ea90d8509618caaff1e0211bd937704a4394e
selsiofy <112800497+selsiofy@users.noreply.github.com>
2022-10-18 07:13:33 +0200

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,42 @@
+data "azurerm_subscription" "current" {
+}
+
+data "azurerm_resource_group" "bastion" {
+  name = var.resource_group_name
+}
+
+data "azurerm_subnet" "Azure_Bastion_Subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+}
+
+data "azurerm_subnet" "vm_subnet" {
+ name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = "vnet-name"
+  resource_group_name  = var.resource_group_name  
+subnet {
+    name           = "AzureBastionSubnet"
+    #address_prefix = 
+  }
+}
+
+
+locals {
+locations_short = {
+    #locations
+  }
+
+  environments_short = {
+   #env
+  }
+#location_short    = 
+#environment       = 
+#environment_short = 
+#zones             = 
+}
