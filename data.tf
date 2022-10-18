b7dbd855702201626095b1b4630d4f518bb08fea
selsiofy <112800497+selsiofy@users.noreply.github.com>
2022-10-18 13:30:21 +0200

    Update data.tf

--- data.tf
+++ data.tf
@@ -6,24 +6,24 @@ data "azurerm_resource_group" "bastion" {
 }
 
 data "azurerm_subnet" "Azure_Bastion_Subnet" {
-  name                 = var.subnet_name
+  name                 = "AzureBastiuonSubnet"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
 }
 
 data "azurerm_subnet" "vm_subnet" {
- name                 = var.subnet_name
+ name                  = var.subnet_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
 }
 
 data "azurerm_virtual_network" "vnet" {
   name                = "vnet-name"
   resource_group_name  = var.resource_group_name  
-subnet {
-    name           = "AzureBastionSubnet"
+#subnet {
+ #   name           = "AzureBastionSubnet"
     #address_prefix = 
-  }
+  #}
 }
 
 
